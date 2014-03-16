33268fdb65b33b24c0ec157457179a2fc45e01ce
Krzysztow <Krzysztow@gmail.com>
2014-03-16 07:46:13 +0000

    Improved (I think so) merger.

--- src/org/dejave/util/SortMerger.java
+++ src/org/dejave/util/SortMerger.java
@@ -7,134 +7,183 @@ import java.util.ListIterator;
 
 public abstract class SortMerger<T> {
 	Comparator<T> comparator = null;
-
+	T firstValue = null;
+	T secondValue = null;
+	
+	/**
+	 * Constructs new merger with a given comparator.
+	 * @param comparator
+	 */
 	public SortMerger(Comparator<T> comparator) {
 		this.comparator = comparator;
 	}
 
+	/**
+	 * Invoked when there are two values from different inputs to be merged.
+	 * @param first - value of the first input;
+	 * @param second - value of the second intput.
+	 */
 	public abstract void mergeValues(T first, T second);
+	
+	/**
+	 * Invoked to create new iterator at positionIdx.
+	 * @param positionIdx index of the first element to be returned from the list iterator (by a call to next)
+	 * @return
+	 */
 	public abstract ListIterator<T> createFirstIteratorAt(int positionIdx);
 
 	public void doMerge(ListIterator<T> firstIt, ListIterator<T> secondIt) {
-		boolean firstGroupStarted = false;
-		boolean firstInputEnded = false;
+		//first and last index of (inclusive) of equal-values group in the first input
 		int firstGroupStartIdx = 0;
-
+		int firstGroupLastIdx = 0;
+		//value of the equal-values group
 		T groupValue = null;
-		T firstValue = firstIt.next();
-		T secondValue = secondIt.next();
+		
+		//if some of the inputs has no values, we are done
+		if (!firstIt.hasNext() || !secondIt.hasNext())
+			return;
+			
+		//otherwise set them to first positions
+		firstValue = firstIt.next();
+		secondValue = secondIt.next();
 
+		int ret = -1;//something different than 0, to force first comparison
 		do {
-			int ret = comparator.compare(firstValue, secondValue);
 			if (0 != ret) {
-				if (firstGroupStarted) {
-					if (! firstInputEnded) {
-						if (secondIt.hasNext())
-							secondValue = secondIt.next();
-						else {
-							break;
-						}
-					}
-
-					int ret2 = comparator.compare(groupValue, secondValue);
-					if (0 == ret2) {
-						firstIt = createFirstIteratorAt(firstGroupStartIdx);
-						firstValue = firstIt.next();
-						assert(firstValue == groupValue);
-						//we could make a join here with this element
-					}
-					else {
-						firstGroupStarted = false;
-					}
-				}
-				else {
-					if (ret < 0) {
-						if (firstIt.hasNext()) {
-							firstValue = firstIt.next();
-						}
-						else 
-							break;
-					}
-					else if (ret > 0) {
-						if (secondIt.hasNext())
-							secondValue = secondIt.next();
-						else
-							break;
-					}
-				}
+				ret = comparator.compare(firstValue, secondValue);
 			}
 			else {
+				//values are same - merge them
 				mergeValues(firstValue, secondValue);
 
-				if (! firstGroupStarted) {
-					firstGroupStarted = true;
-					firstGroupStartIdx = firstIt.previousIndex();
-					groupValue = firstValue;
-				}
+				//remember equal-values parameters
+				firstGroupStartIdx = firstIt.previousIndex();
+				groupValue = firstValue;
 
-				if (firstIt.hasNext()) {
+				int cmpRes = 0;
+				//iterate over the first tuples until the different one is found
+				while (firstIt.hasNext()) {
 					firstValue = firstIt.next();
+					cmpRes = comparator.compare(firstValue, groupValue);
+					if (0 == cmpRes) {
+						//we are still in an equal-values group -> second tuple is also equal to current first input, thus merge
+						mergeValues(firstValue, secondValue);
+					}
+					else 
+						break;
 				}
-				else if (secondIt.hasNext()) {
+				
+				//reached the end of a group, two cases may occur
+				//	- end of the group coincides with end of the input stream (!hasNext() and 0 == cmpRes). Then group is [firstGroupStartIdx, firstIt.nextIndex()] (inclusive)
+				//	- otherwise, then group is [firstGroupStartIdx, firstIt.previousIndex()] (inclusive)
+				boolean firstIsDone = !firstIt.hasNext() && 0 == cmpRes;
+				firstGroupLastIdx = firstIsDone ? firstIt.nextIndex() : firstIt.previousIndex();
+				
+				while (secondIt.hasNext()) {
 					secondValue = secondIt.next();
-					firstIt = createFirstIteratorAt(firstGroupStartIdx);
-					firstValue = firstIt.next();
-					firstInputEnded = true;
+					//if the following second input values are equal to the equal-values group value, merge them without value checking against first (since they are equal)
+					cmpRes = comparator.compare(groupValue, secondValue);
+					if (0 == cmpRes) {
+						if (firstGroupStartIdx == firstGroupLastIdx ) {//no need to create iterator
+							mergeValues(groupValue, secondValue);
+						}
+						else {//more items in a group to iterate
+							ListIterator<T> fIt = createFirstIteratorAt(firstGroupStartIdx);
+							while (fIt.nextIndex() < firstGroupLastIdx) {
+								mergeValues(fIt.next(), secondValue);
+							}
+						}
+					}
+					else 
+						break;
 				}
-				else {
+				
+				//if the first input was done and there is nothing in the second one, we are done
+				if (firstIsDone && !secondIt.hasNext()) 
 					break;
-				}
+				
+				//one of the vales was changed, perform another comparison
+				ret = comparator.compare(firstValue, secondValue);
 			}
-		} while (true);
+		} while (advancePointers(firstIt, secondIt, ret));
 	}
-	
+
+	private boolean advancePointers(ListIterator<T> firstIt, ListIterator<T> secondIt, int ret) {
+		//if values different, increment smaller one, unless its input is finished - then we're done too
+		if (ret < 0) {
+			if (firstIt.hasNext()) {
+				firstValue = firstIt.next();
+			}
+			else 
+				return false;
+		}
+		else if (ret > 0) {
+			if (secondIt.hasNext())
+				secondValue = secondIt.next();
+			else
+				return false;
+		}
+		return true;
+	}
+
 	/**
 	 * ########################################
 	 * ##### Debug
 	 * ########################################
 	 */
-	
+
 	//class which allows strings to be distinguished with idx
 	static class DebugTuple {
 		String s;
 		Integer idx;
-		
+
 		public DebugTuple(String s, Integer idx) {
 			this.s = s;
 			this.idx = idx;
 		}
-		
+
 		@Override
 		public String toString() {
 			return s + " (" + idx + ")";
 		}
 	}
-	
+
 	//comparator compares Tuple strings only
 	static public class DebugTupleComparator implements Comparator<DebugTuple> {
+		int cmpCnt = 0;
 		@Override
 		public int compare(DebugTuple v0, DebugTuple v1) {
+			cmpCnt++;
 			return v0.s.compareTo(v1.s);
 		}
+		
+		public int lastCmpCnt() {
+			int tmp = cmpCnt;
+			cmpCnt = 0;
+			return tmp;
+		}
 	}
-	
+
 	//
 	public static class DebugTupleSortMerger extends SortMerger<DebugTuple> {
 		ArrayList<String> result = new ArrayList<String>(); 
+		DebugTupleComparator comparator = null;
 		List<DebugTuple> first = null;
 		List<DebugTuple> second = null;
 
 		public DebugTupleSortMerger(List<DebugTuple> first, List<DebugTuple> second) {
-			super(new DebugTupleComparator());
+			super(null);
+			comparator = new DebugTupleComparator();
+			super.comparator = comparator;
 			this.first = first;
 			this.second = second;
 		}
-		
+
 		@Override
 		public void mergeValues(DebugTuple first, DebugTuple second) {
 			result.add(first.s + "_" + first.idx + " : " + second.s + "_" + second.idx);
 		}
-		
+
 		public List<String> result() {
 			return this.result;
 		}
@@ -144,42 +193,46 @@ public abstract class SortMerger<T> {
 			return first.listIterator(positionIdx);
 		}
 		
+		public int cmpCnt() {
+			return comparator.lastCmpCnt();
+		}
+
 	}
-	
-    /**
-     * Debug main.
-     */
-    public static void main (String [] args) {
+
+	/**
+	 * Debug main.
+	 */
+	public static void main (String [] args) {
 		//normal case
 		String a[] = {"A", "A", "B", "B", "J", "J", "K"};
 		String b[] = {"B", "C", "G", "H", "I", "I", "J", "J", "J", "M"};
 		checkMergeOfArrays(a, b, 8);
 		checkMergeOfArrays(b, a, 8);
-		
+
 		//end of group == end of one input
 		String c[] = {"A", "B", "B"};
 		String d[] = {"A", "B"};
 		checkMergeOfArrays(c, d, 3);
 		checkMergeOfArrays(d, c, 3);
-		
+
 		//same as above, but longer B group end of group == end of one input
 		String c1[] = {"A", "B", "B", "B"};
 		String d1[] = {"A", "B"};
 		checkMergeOfArrays(c1, d1, 4);
 		checkMergeOfArrays(d1, c1, 4);
-		
+
 		//beginning/end of group == beginning/end of input; one input takes all the array
 		String e[] = {"J", "J", "K"};
 		String f[] = {"J", "J", "J"};
 		checkMergeOfArrays(e, f, 6);
 		checkMergeOfArrays(f, e, 6);
-		
+
 		//beginning of group == beginning of input
 		String g[] = {"A", "A", "B"};
 		String h[] = {"A", "B"};
 		checkMergeOfArrays(g, h, 3);
 		checkMergeOfArrays(h, g, 3);
-		
+
 		//I should learn how to run Java
 		boolean handlesExceptions = false;
 		try { assert(false); }
@@ -190,6 +243,9 @@ public abstract class SortMerger<T> {
 			System.err.println("ERROR: add -ea VM flags, to test for errors");
 		}
 		
+		System.out.println("############################");
+		System.out.println("TEST DONE!");
+
 	}//main()
 
 	private static void checkMergeOfArrays(String a[], String b[], int joinsNo) {
@@ -198,15 +254,15 @@ public abstract class SortMerger<T> {
 
 		System.out.println("Merging: " + first);
 		System.out.println("with   : " + second);
-		
+
 		DebugTupleSortMerger merger = new DebugTupleSortMerger(first, second);
 		merger.doMerge(first.listIterator(), second.listIterator());
 		List<String> result = merger.result();
+		System.out.println("DONE: " + result + ": " + joinsNo + " ?= " + result.size() + " with comparisons " + merger.cmpCnt());
 		assert(result.size() == joinsNo);
-		System.out.println("DONE: " + result + ": " + joinsNo + " ?= " + result.size());
 		System.out.println("");
 	}
-	
+
 	private static List<DebugTuple> createTupleList(String[] a) {
 		List<DebugTuple> res = new ArrayList<DebugTuple>();
 		int interIdx = 0;
@@ -219,7 +275,7 @@ public abstract class SortMerger<T> {
 			}
 			res.add(new DebugTuple(a[i], interIdx));
 		}
-		
+
 		return res;
 	}
 }
